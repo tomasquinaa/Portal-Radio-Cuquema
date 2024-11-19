0223283960a53377e37a4860d236f59e06862aa8
tomasquinaa <armando.quinanga2023@gmail.com>
2024-11-19 16:01:00 +0100

    feat: atualizado o link no logo

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
 import { exportImg } from "../utils/exportImg";
-
+import { routes } from "../utils/menu";
 
 export const HeaderTop = () => {
   return (
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
       <div className="flex items-center space-x-4">
         {/* Logotipo com melhorias */}
-        <img 
-          src={exportImg.logotipo} 
-          alt="Logo" 
-          className="w-36 h-auto object-contain transition-transform transform hover:scale-105 duration-300" 
-        />
+        <Link to={routes.MENU_INICIO}>
+          <img
+            src={exportImg.logotipo}
+            alt="Logo"
+            className="w-36 h-auto object-contain transition-transform transform hover:scale-105 duration-300"
+          />
+        </Link>
       </div>
     </header>
   );
